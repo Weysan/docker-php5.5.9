d80f8e428fadd341ce167e51330f9f0587791149
Raphaël GONCALVES <contact@raphael-goncalves.fr>
2015-12-28 23:13:26 +0100

    add aliases
    phpunit, composer and php aliases

--- bootlocal.sh
+++ bootlocal.sh
@@ -5,4 +5,6 @@ mkdir /var/www
 mount -t vboxsf -o uid=33,gid=33 www /var/www
 
 echo 'alias composer='\''docker run -ti --rm -v $(pwd):/app composer/composer'\''' >> /home/docker/.ashrc
+echo 'alias php='\''docker exec -ti docker_web_1 php'\''' >> /home/docker/.ashrc
+echo 'alias phpunit='\''docker run -v $(pwd):/app phpunit/phpunit'\''' >> /home/docker/.ashrc
 echo 'cd /var/www' >> /home/docker/.ashrc
